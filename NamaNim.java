125f51d6890c5a73dceed67442b8555f63c54e43
thessaroxana <thessaroxana3@gmail.com>
2021-09-27 16:53:21 +0700

    first commit

---
+++ NamaNim.java
@@ -0,0 +1,8 @@
+public class NamaNim {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Thessa Roxana");
+		System.out.println("NIM	 : 6706213025");
+		System.out.println("Kelas: D3IF45-04");
+	}
+}
